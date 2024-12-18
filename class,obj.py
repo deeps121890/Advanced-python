2854f37b6d0b7862c863b1909fc92dd451255a71
deeps121890 <deepikadeepu8818@gmail.com>
2024-12-18 16:10:03 +0530

    Create class,obj.py

---
+++ class,obj.py
@@ -0,0 +1,38 @@
+'''
+class Stu: 
+    def m1(self):  
+        print('hello')
+'''
+class Details:
+    def det(self,x,y):
+        print('x=',x)
+        print('y=',y)
+s=Details()  
+s.det(20,'apple') 
+'''
+o/p: x=20
+     y=apple
+'''
+class Stu:
+        def m1(self):
+             print('hello')
+    s=Stu()
+    s.m1()
+'''
+o/p: hello
+'''
+subject = (input())
+marks = (int(input()))
+class Academics:
+    def acd(self,subject,marks):
+        print('subject:',subject)
+        print('marks:',marks)
+s=Academics()
+s.acd(subject,marks)
+'''
+o/p:
+subject: maths
+marks:100
+
+
+
