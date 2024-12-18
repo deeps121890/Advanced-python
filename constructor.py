bbf8bac239bad0af1eed0767a975af05933b3f86
deeps121890 <deepikadeepu8818@gmail.com>
2024-12-18 16:12:07 +0530

    Create constructor.py

---
+++ constructor.py
@@ -0,0 +1,41 @@
+'''
+class PythonDeveloper:
+    def __init__(selfself,number,name):
+        print('Python developer')
+        print('number:',number)
+        print('name:',name)
+one=PythonDeveloper(1,'python')
+'''
+o/p:
+ Python developer
+number: 1
+name: python
+'''
+class Details:
+    def __init__(self):
+        print('Details')
+        print('name:','deepika')
+        print('age:',10)
+        print('location:','palamaner')
+one=Details()
+print('\n')
+class Details:
+    def __init__(self,name,age,location):
+        print('Details')
+        print('name:', name)
+        print('age:', age)
+        print('location:',location)
+two = Details('deepika',20,'palamaner')
+'''
+o/p:
+Details
+name: deepika
+age: 10
+location: palamaner
+
+
+Details
+name: deepika
+age: 20
+location: palamaner
+
