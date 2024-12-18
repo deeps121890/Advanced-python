7f938ec120a21aefe402568e84dfb14ca3cc7e25
deeps121890 <deepikadeepu8818@gmail.com>
2024-12-18 16:20:20 +0530

    Create instance var.py

---
+++ instance var.py
@@ -0,0 +1,17 @@
+'''
+class Test:
+    def f1(self):
+        self.x=10
+        print(self.x)
+    def f2(self):
+        self.y=20
+        print(self.y)
+t1=Test()
+t1.f1()
+t1.f2()
+print(t1.f1)
+print(t1.f2)
+'''
+o/p:
+10
+20
